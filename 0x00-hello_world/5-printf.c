fb65192f3aeb213811a6a1bd2fbfa62c8587da56
yinmi1 <egeregorweyinmi@gmail.com>
2023-03-16 08:02:53 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main fuction
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with the proper grammer, but the outcome os a piece of art,");
+	return (0);
+}
