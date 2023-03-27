3a0b3b98e0110faf1a3c52ee99086fa3cb5abc5f
yinmi1 <egeregorweyinmi@gmail.com>
2023-03-27 12:46:10 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <limits.h>
+
+int _strlen(char *s);
+int _putchar(char c);
+int _atoi(char *s);
+
+char *_strcpy(char *dest, char *src);
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str); 
+void print_array(int *a, int n);
+
+#endif
